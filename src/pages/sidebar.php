0cb0f05472fa7d3f4f0d4c211918540ecf62e4da
Alice Jacka <harry@jacka.net.au>
2018-11-27 14:58:34 +1100

    scoreboard update

--- src/pages/sidebar.php
+++ src/pages/sidebar.php
@@ -7,9 +7,17 @@
 
 <?php
 
-for ($i=0; $i < 10; $i++) {
+echo '<tr class="leaderNo0"><td>'. $scoreboard[0][0] .' is da best</td><td>'. $scoreboard[0][1] .'</td></tr>';
+
+$j = 1;
+for ($i=1; $i < 10; $i++) {
+    $j++;
+    if ($j > 9)
+    {
+        $j = 1;
+    }
     if (isset($scoreboard[$i])) {
-        echo '<tr class="leaderNo'. $i .'"><td>'. $scoreboard[$i][0] .'</td><td>'. $scoreboard[$i][1] .'</td></tr>';
+        echo '<tr class="leaderNo'. $j .'"><td>'. $scoreboard[$i][0] .'</td><td>'. $scoreboard[$i][1] .'</td></tr>';
     }
 }
 
